1aacd98b310d16fde678dec0c18d86232e76c876
bullborncontact-png <bullborncontact@gmail.com>
2026-08-21 16:28:22 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB37IY26RQLCwYFB4U_biSI7FslS1GuWSA",
+  authDomain: "bullborn-f7192.firebaseapp.com",
+  databaseURL: "https://bullborn-f7192-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bullborn-f7192",
+  storageBucket: "bullborn-f7192.firebasestorage.app",
+  messagingSenderId: "610156006264",
+  appId: "1:610156006264:web:01a13091d3b719100f168d",
+  measurementId: "G-2TBS7JZFTC"
+};
